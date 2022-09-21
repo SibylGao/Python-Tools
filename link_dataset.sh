c68a51ff0fcfea19778eb493f53e47e7240e8576
一条冻狗 <42674322+SibylGao@users.noreply.github.com>
2022-09-21 12:01:39 +0800

    Create link_dataset.sh

---
+++ link_dataset.sh
@@ -0,0 +1,7 @@
+mkdir data
+cd data
+mkdir kitti
+cd ..
+ln -s /home/ssd2/ssd2/gaoshiyu2ssd/kitti/monodetr_kitti/training/ ./data/kitti/
+ln -s /home/ssd2/ssd2/gaoshiyu2ssd/kitti/monodetr_kitti/testing/ ./data/kitti/
+ln -s /home/ssd2/ssd2/gaoshiyu2ssd/kitti/monodetr_kitti/ImageSets/ ./data/kitti/
